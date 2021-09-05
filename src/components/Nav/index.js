ea730acc502f9293b80bb5c38492b10b81bb98d1
Sona <sonali23.1293@gmail.com>
2021-09-05 19:36:31 -0400

    nav test

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -26,23 +26,23 @@ function Nav() {
         },
     ]
     return (
-        <header>
+        <header className="flex-row px-1">
             <h2>
-                <a href="/">
+                <a data-testid="link" href="/">
                     <span role="img" aria-label="camera">📸</span> Oh Snap!
                 </a>
             </h2>
             <nav>
                 <ul className="flex-row">
                     <li className="mx-2">
-                        <a href="#about">About me</a>
+                        <a data-testid="about" href="#about">About me</a>
                     </li>
                     <li>
                         <span>Contact</span>
                     </li>
 
                     {categories.map((category) => (
-                    <li classsName="mx-1" key={ category.name } >
+                    <li className="mx-1" key={ category.name } >
                         <span onClick={ () => categorySelected(category.name) }>{ category.name }</span>
                     </li>
                     ))}
